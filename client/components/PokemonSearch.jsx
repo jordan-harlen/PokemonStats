0d0c14359d3435c0fa93a8913cf16b56621fdbfe
Jordan Harlen <jordan_harlen@hotmail.com>
2022-10-12 16:33:46 +1300

    added stats and logo

--- client/components/PokemonSearch.jsx
+++ client/components/PokemonSearch.jsx
@@ -6,7 +6,27 @@ import { getAllPokemonThunk } from '../actions/pokemonActions'
 const PokemonSearch = () => {
   const allPokemon = useSelector((redux) => redux.pokemon)
   const dispatch = useDispatch()
+
   const [pokemonName, setPokemonName] = useState('')
+  const [pokemonChosen, setPokemonChosen] = useState(false)
+  const [pokemon, setPokemon] = useState({
+    name: '',
+    species: '',
+    img: '',
+    hp: '',
+  })
+
+  useEffect(() => {
+    const searchedPokemon = {
+      name: pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1),
+      species: allPokemon?.species?.name,
+      img: allPokemon?.sprites?.front_default,
+      hp: allPokemon?.stats ? allPokemon?.stats[0].base_stat : '',
+      atk: allPokemon?.stats ? allPokemon?.stats[1].base_stat : '',
+      def: allPokemon?.stats ? allPokemon?.stats[2].base_stat : '',
+    }
+    setPokemon(searchedPokemon)
+  }, [allPokemon])
 
   function handleChange(e) {
     e.preventDefault()
@@ -16,15 +36,18 @@ const PokemonSearch = () => {
   function handleSubmit(e) {
     e.preventDefault()
     dispatch(getAllPokemonThunk(pokemonName))
+    setPokemonChosen(true)
     document.getElementById('search').value = ''
-    console.log('submitted ' + pokemonName)
   }
 
   return (
     <>
       <div className="PokemonList">
         <div className="TitleSection">
-          <h2>Pokemon!</h2>
+          <img
+            src="https://fontmeme.com/permalink/221012/807552d58592f4c4b9f6fa4b43fd08d7.png"
+            alt="title logo"
+          />
           <input
             id="search"
             type="text"
@@ -42,11 +65,21 @@ const PokemonSearch = () => {
           </button>
         </div>
       </div>
-
       <div className="Pokemon">
-        <img src={allPokemon?.sprites?.front_default} alt={allPokemon?.name} />
-        <h2>{allPokemon?.name}</h2>
-        <p>allPokemon?.stats[0]</p>
+        {!pokemonChosen ? (
+          <h2>Please choose a Pokemon</h2>
+        ) : (
+          <>
+            <h2>Choose another Pokemon!</h2>
+            <img src={pokemon?.img} alt={pokemon?.name} />
+            <h2>{pokemon?.name}</h2>
+            <ul>
+              <li>HP: {pokemon?.hp}</li>
+              <li>ATK: {pokemon?.atk}</li>
+              <li>DEF: {pokemon?.def}</li>
+            </ul>
+          </>
+        )}
       </div>
     </>
   )
